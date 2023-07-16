7652e1aac563d91273e736a105678517a184d99c
ajcar110 <ajcar110@gmail.com>
2023-07-16 14:36:59 -0500

    added bad bullet delay

--- Abilities/Shoot/Shoot.gd
+++ Abilities/Shoot/Shoot.gd
@@ -2,6 +2,8 @@ extends Ability
 
 @export var number_of_projectiles: int = 1
 
+
 func Execute():
 	for i in number_of_projectiles:
+		await get_tree().create_timer(.05).timeout
 		Utils.spawn_projectile(source,"small",self.global_position)
